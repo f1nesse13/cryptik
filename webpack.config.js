a60aadc035a8c15fd3cff68c6af33df8b547491d
f1nesse13 <philly.teg@gmail.com>
2022-05-08 20:59:22 -0400

    configure prettier single quotes

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,28 @@
-const path = require("path")
-const HtmlWebpackPlugin = require("html-webpack-plugin")
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-	entry: path.join(__dirname, "src", "index.js"),
-	output: { path: path.join(__dirname, "build"), filename: "index.bundle.js" },
-	mode: process.env.NODE_ENV || "development",
-	resolve: { modules: [path.resolve(__dirname, "src"), "node_modules"] },
-	devServer: { static: path.join(__dirname, "src"), open: true },
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				exclude: /node_modules/,
-				use: ["babel-loader"],
-			},
-			{
-				test: /\.(css|scss)$/,
-				use: ["style-loader", "css-loader"],
-			},
-		],
-	},
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.join(__dirname, "src", "index.html"),
-		}),
-	],
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: { path: path.join(__dirname, 'build'), filename: 'index.bundle.js' },
+  mode: process.env.NODE_ENV || 'development',
+  resolve: { modules: [path.resolve(__dirname, 'src'), 'node_modules'] },
+  devServer: { static: path.join(__dirname, 'src'), open: true },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+      {
+        test: /\.(css|scss)$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+  ],
 }
